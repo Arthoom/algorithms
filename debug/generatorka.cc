a99c20f6b3ecba2b52c093d8aff90989cbdbd3b1
Arthoom <arthoom@puchalski.pro>
2017-12-28 11:57:37 +0100

    Update generatorka.cc

--- debug/generatorka.cc
+++ debug/generatorka.cc
@@ -3,7 +3,10 @@
 mt19937_64 gen__(chrono::system_clock::now().time_since_epoch().count());
 LL rd(LL a, LL b) { return uniform_int_distribution<LL>(a, b)(gen__); }
 
-int main() {
-	cout << rd(0, 100);
+int main(int argc, char* argv[]) {
+	if(argc != 2)
+		cout << "invalid argc\n";
+	int xd = atoi(argv[1]);
+	cout << rd(0, xd);
 	return 0;
 }
